98b773d5aede9390a74d49579f20b8927c8bf3ce
Yoav Tsook <yoav.tsook@celsius.network>
2022-03-17 15:26:39 +0200

    always arrow

--- src/SingleGuess.js
+++ src/SingleGuess.js
@@ -8,9 +8,9 @@ export const SingleGuess = ({playerId, rightPlayerId}) => {
 
   const renderColumn = (text, color, arrow) => {
     return (
-      <div style={{flex: 1, margin: 5, padding: 3, border: "1px solid black", borderRadius: 10, backgroundColor: color}}>
-        {arrow === "down" && <FaArrowAltCircleDown/>}
-        {arrow === "up" && <FaArrowAltCircleUp/>}
+      <div style={{flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 5, padding: 3, border: "1px solid black", borderRadius: 10, backgroundColor: color}}>
+        {arrow === "down" && <FaArrowAltCircleDown style={{marginRight: 8}}/>}
+        {arrow === "up" && <FaArrowAltCircleUp style={{marginRight: 8}}/>}
         <span>{text}</span>
       </div>
     )
@@ -22,7 +22,7 @@ export const SingleGuess = ({playerId, rightPlayerId}) => {
       <div
         style={{display: 'flex', flexDirection: "row", justifyContent: 'center', alignItems: 'center'}}>
         {renderColumn(playerData["קבוצה"], playerData["קבוצה"] === rightPlayerData["קבוצה"] ? "green" : "grey")}
-        {renderColumn(playerData["גיל"], playerData["גיל"] === rightPlayerData["גיל"] ? "green" : (Math.abs(playerData["גיל"] - rightPlayerData["גיל"]) < 3 ? "yellow" : "grey"), Math.abs(playerData["גיל"] - rightPlayerData["גיל"]) < 3 ? (playerData["גיל"] > rightPlayerData["גיל"] ? "down" : "up") : undefined)}
+        {renderColumn(playerData["גיל"], playerData["גיל"] === rightPlayerData["גיל"] ? "green" : (Math.abs(playerData["גיל"] - rightPlayerData["גיל"]) < 3 ? "yellow" : "grey"), playerData["גיל"] !== rightPlayerData["גיל"] ? (playerData["גיל"] > rightPlayerData["גיל"] ? "down" : "up") : undefined)}
         {renderColumn(playerData["ישראלי/זר"], playerData["ישראלי/זר"] === rightPlayerData["ישראלי/זר"] ? "green" : "grey")}
         {renderColumn(playerData["פלייאוף"], playerData["פלייאוף"] === rightPlayerData["פלייאוף"] ? "green" : "grey")}
         {renderColumn(playerData["עמדה"], playerData["עמדה"] === rightPlayerData["עמדה"] ? "green" : "grey")}
